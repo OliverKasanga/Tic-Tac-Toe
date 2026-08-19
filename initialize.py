50e84952fbd8d5c50f53f60376c95a8d0db73807
NOEL KAY <prolificnoa@gmail.com>
2026-08-19 12:49:28 +0300

    Describe your changes

---
+++ initialize.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import messagebox
+
+root = tk.Tk()
+root.title("TIC-TAC-TOE")
+root.geometry("300x200")
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+mark = ''
+
+count = 0
+
+panels = ['panel', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
+
+root.mainloop()
